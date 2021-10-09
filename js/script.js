298af291487881ac5a7ecedef3acd30d82082dd4
Igo Gomes <igogomes0@gmail.com>
2021-10-09 02:19:30 -0300

    Inclusão de códigos para envio de mensagens através de formulários

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 if(window.SimpleSlide) {
-
     new SimpleSlide({
         slide: "quote",
         time: 5000
     });
-
     new SimpleSlide({
         slide: "portfolio",
         time: 5000,
         nav: true
     });
-
 }
 
 if(window.SimpleAnime) {
-
     new SimpleAnime();
+}
 
+if(window.SimpleForm) {
+    new SimpleForm({
+        form: ".formphp",
+        button: "#enviar",
+        erro: "<div id='form-erro'><h2>Erro no envio</h2><p>Um erro ocorreu, tente enviar sua mensagem para contato@bikcraft.com.</p></div>",
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve retornaremos seu contato.</p></div>"
+    });
 }
